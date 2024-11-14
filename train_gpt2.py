0cdcfc5304029a9c6b0089c32ebcee52794ff18f
hrithiksagar <hrithiksagar36@gmail.com>
2024-11-14 15:28:55 +0530

    Updated 2nd block (Block class)

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,6 +3,21 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F 
 
+#### The below part is written second
+class Block(nn.Module):
+    def __init__(self.config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+    
+    def foward(self, x):
+        x = x+ self.attn(self.ln_1(x))
+        x = x+self.attn(self.ln_2(x))
+        return x
+
+##### The below part is written First
 @dataclass
 class GPTConfig:
     block_size: int = 256
@@ -19,7 +34,7 @@ class GPT(nn.Module):
         
         # wte = weight, token mebeddings
         # pe = posiitonal embeddings 
-        # this is the skeleton that represents original transformer architecture with few tweakings in the positions of the Attention heads, linear layers and multi head attensions 
+        # This is the skeleton that represents original transformer architecture with few tweakings in the positions of the Attention heads, linear layers and multi head attensions 
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             wpe = nn.Embedding(config.block_size, config.n_embd),
